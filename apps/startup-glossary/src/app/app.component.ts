0155509f976a0244c8110c4c98fb5eef3e647776
Jedidiah Weller <jedi@openforge.io>
2021-11-21 18:06:08 -0500

    build(): finishing setup for nx lint and nx format

--- apps/startup-glossary/src/app/app.component.ts
+++ apps/startup-glossary/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'openforge-academy-root',
+    selector: 'of-academy-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
